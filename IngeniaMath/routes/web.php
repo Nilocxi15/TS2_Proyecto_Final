f5b7f32b14e75197c419a516575f5451b5e97481
Javier Ixcolin <javierixcolin202132163@cunoc.edu.gt>
2026-04-16 21:06:23 -0600

    Feat Login
    Se agregó frontend para la parte de login, además del enlace para el registro

--- IngeniaMath/routes/web.php
+++ IngeniaMath/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Rutas para login y autenticación
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Rutas para registro de usuarios nuevos
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
